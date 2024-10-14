0028e756e6f85da77e9b26c4799d560b20b08b12
Muharrem Tekin <muharremtekinn@outlook.com.tr>
2024-10-14 21:24:30 +0300

    Refactor login page component to handle user login functionality and display error popups

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,16 +10,18 @@ const Page: React.FC = () => {
     const [showPassword, setShowPassword] = useState<boolean>(false);
     const [username, setUsername] = useState<string>('');
     const [password, setPassword] = useState<string>('');
-    const [errorMessage, setErrorMessage] = useState(''); // State for error message
-    const [showPopup, setShowPopup] = useState(false); // State for popup visibility
     const { login } = useAuth();
     const router = useRouter();
+    const [errorMessage, setErrorMessage] = useState(''); // State for error message
+    const [showPopup, setShowPopup] = useState(false); // State for popup visibility
 
-    const togglePasswordVisibility = () => setShowPassword((prev) => !prev);
+
 
     const closePopup = () => {
         setShowPopup(false);
     };
+
+    const togglePasswordVisibility = () => setShowPassword((prev) => !prev);
 
     const handleSignIn = async (e: React.FormEvent) => {
         e.preventDefault();
@@ -27,7 +29,7 @@ const Page: React.FC = () => {
             if (!username || !password) {
                 setErrorMessage('Username and password are required.');
                 setShowPopup(true);
-                return; // Prevent API request if validation fails
+                return;
             }
 
             const response = await api.post('/tokens', {
@@ -37,30 +39,19 @@ const Page: React.FC = () => {
 
             if (response.status === 200) {
                 const data = response.data;
-                login(data.token); // Store the token securely using your login method
-                router.push('/profile/' + data.userName); // Redirect to the user's profile
-            } else {
-                setErrorMessage('Login failed. ' + response.data.Message);
-                setShowPopup(true);
-                console.error(response.data.Message);
+                login(data.token);
+                router.push('/profile/' + data.userName);
             }
-        } catch (error) {
-            setErrorMessage('Login failed. ' + (error as any).response.data.Message);
+
+        } catch (error: any) {
+            setErrorMessage('Login failed. ' + error.response.data.Message);
             setShowPopup(true);
-            console.error('Error during login: ', (error as any).response.data.Message);
+            console.error('Error during login: ', error.response.data.Message);
         }
     };
 
     return (
         <div className="relative h-screen">
-            {/* Arka Plan Resmi ve Blur Efekti */}
-            {/* <div
-                className="absolute inset-0 bg-cover bg-center filter blur-sm z-0"
-                style={{ backgroundImage: "https://www.pexels.com/tr-tr/fotograf/deniz-kent-sehir-peyzaj-27230180/" }}
-            ></div> */}
-
-            {/* <SideImage /> */}
-            {/* Ortalanmış LoginForm */}
             <div className="relative flex items-center justify-center  z-10 mt-2">
                 <LoginForm
                     showPassword={showPassword}
