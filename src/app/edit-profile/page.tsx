4db4574c7b04a9267bdd71607b8c270b4e67917a
Muharrem Tekin <muharremtekinn@outlook.com.tr>
2024-10-14 21:24:26 +0300

    Refactor EditProfilePage component to handle user profile updates, social link updates, qualifications updates, and password updates

--- src/app/edit-profile/page.tsx
+++ src/app/edit-profile/page.tsx
@@ -1,39 +1,27 @@
-// pages/edit-profile.tsx
 "use client";
-import { Qualification, QualificationType, SocialLinkType, UserData } from "@/types/user";
-import axios from "axios";
+
+import { UserData } from "@/types/user";
 import { useRouter } from 'next/navigation';
 import { useEffect, useState } from "react";
-import { FaRegEdit } from "react-icons/fa";
-import { FaEyeSlash, FaEye, FaLinkedin, FaMedium, FaSquareGithub, FaSquareInstagram, FaSquareXTwitter, FaYoutube } from "react-icons/fa6";
-import { LuPlusCircle } from "react-icons/lu";
-import { RiDeleteBin6Line } from "react-icons/ri";
 import Header from "../components/common/header";
-import Image from "next/image";
-import SocialMediaInput from "../components/common/social-media-input";
 import api from "@/services/api";
-import ProfilePictureSelector from "../components/common/profile-picture-selector";
 import UserProfileForm from "../components/edit-profile-forms/user-informations-form";
 import SocialLinksForm from "../components/edit-profile-forms/social-links-form";
 import QualificationsForm from "../components/edit-profile-forms/qualifications-form";
 import SecuritySettingsForm from "../components/edit-profile-forms/security-settings-form";
+import ErrorPopup from "../components/common/error-popup";
+import PhoneNumbersForm from "../components/edit-profile-forms/phone-numbers-form";
 
 const EditProfilePage: React.FC = () => {
     const router = useRouter();
     const [userData, setUserData] = useState<UserData | null>(null);
-    const [formData, setFormData] = useState<UserData | null>(null);
-    const [isMatching, setIsMatching] = useState<boolean | null>(null);
-    const [showPassword, setShowPassword] = useState({
-        lastPassword: false,
-        newPassword: false,
-    });
-    const [passwords, setPasswords] = useState({
-        lastPassword: '',
-        newPassword: '',
-        confirmPassword: '',
-    });
 
+    const [errorMessage, setErrorMessage] = useState(''); // State for error message
+    const [showPopup, setShowPopup] = useState(false); // State for popup visibility
 
+    const closePopup = () => {
+        setShowPopup(false);
+    };
 
     // Sayfa yüklendiğinde kullanıcı verilerini çek
     useEffect(() => {
@@ -45,8 +33,8 @@ const EditProfilePage: React.FC = () => {
                 try {
                     const response = await api.get<UserData>('users/me');
                     setUserData(response.data);
-                    setFormData(response.data); // Form verilerini kullanıcı verileriyle doldur
-                } catch (error) {
+                } catch (error: any) {
+                    setErrorMessage(error.response.data.Message);
                     console.error('Error fetching user data:', error);
                     router.push('/login');
                 }
@@ -55,47 +43,9 @@ const EditProfilePage: React.FC = () => {
         }
     }, [router]);
 
-    // Şifre eşleşme kontrolü
-    useEffect(() => {
-        setIsMatching(passwords.newPassword === passwords.confirmPassword);
-    }, [passwords.newPassword, passwords.confirmPassword]);
-
-    // Form verilerini güncelleme
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-        if (formData) {
-            setFormData({
-                ...formData,
-                [e.target.name]: e.target.value,
-            });
-        }
-    };
 
 
-
-
-    // Şifre güncelleme fonksiyonu
-    const handlePasswordUpdate = async () => {
-        const token = localStorage.getItem('authToken');
-        if (token && passwords.lastPassword && passwords.newPassword) {
-            try {
-                await api.put('api/passwords', {
-                    lastPassword: passwords.lastPassword,
-                    newPassword: passwords.newPassword,
-                })
-                alert('Şifre başarıyla güncellendi!');
-                setPasswords({
-                    lastPassword: '',
-                    newPassword: '',
-                    confirmPassword: '',
-                });
-            } catch (error) {
-                console.error('Error changing password:', error);
-                alert('Şifre değiştirilirken bir hata oluştu.');
-            }
-        }
-    };
-
-    if (!formData) {
+    if (!userData) {
         return <div>Loading...</div>;
     }
 
@@ -107,17 +57,25 @@ const EditProfilePage: React.FC = () => {
                 <div className="bg-white my-10 p-4 rounded-lg">
                     {/* Genel Ayarlar */}
 
-                    <UserProfileForm userData={userData}></UserProfileForm>
+                    <UserProfileForm userData={userData} userName={userData.userName} ></UserProfileForm>
                     {/* Sosyal Medya Linkleri */}
                     <SocialLinksForm socialLinks={userData?.socialLinks ?? null}></SocialLinksForm>
 
                     {/* Sertifika Ayarları */}
                     <QualificationsForm qualifications={userData?.qualifications ?? null}></QualificationsForm>
 
+                    {/* Telefon numaraları */}
+                    <PhoneNumbersForm initialPhoneNumbers={userData?.phoneNumbers}></PhoneNumbersForm>
+
                     {/* Güvenlik Ayarları */}
-                    <SecuritySettingsForm/>
+                    <SecuritySettingsForm />
                 </div>
             </div>
+            {showPopup && (
+                <div className="absolute inset-0 flex items-center justify-center z-20">
+                    <ErrorPopup message={errorMessage} onClose={closePopup} />
+                </div>
+            )}
         </div>
     );
 };
